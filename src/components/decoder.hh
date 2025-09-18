70996e176782f9fd61a93c2fd8744fbbcbd35df6
jmsb2022 <jmsb2022@itbdcv-lnx04p.campus.pomona.edu>
2025-09-17 19:14:49 -0700

    autograder test

--- src/components/decoder.hh
+++ src/components/decoder.hh
@@ -38,7 +38,22 @@ class Decoder
     void decode(string instruction, string &operation, int &destination,
                 int &source_register1, int &source_register2, int &imm)
     {
-        // TODO: your implementation here!
+      source_register1 = 0;
+      source_register2 = 0;
+
+      operation = instruction.substr(0, 3);
+
+      if (operation == "ldi") {
+        destination = stoi(instruction.substr(5, 1));
+        imm = stoi(instruction.substr(8, instruction.npos));
+      } else if (operation == "end") {
+        destination = 0;
+        imm = 0;
+      } else {
+        destination = stoi(instruction.substr(5, 1));
+        source_register1 = stoi(instruction.substr(9, 1));
+        source_register2 = stoi(instruction.substr(13, 1));
+      }
     };
 };
 
